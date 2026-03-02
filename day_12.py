cfc8ec631a568edea472d4bfd69f73064c1ab2f3
Kvilo1 <1ej42.gorczyca@gmail.com>
2026-03-03 00:30:40 +0100

    day 12 not yet completed

---
+++ day_12.py
@@ -0,0 +1,55 @@
+# Modules
+import random
+import string
+
+"""
+1. Write a function which generates a six digit/character random_user_id.
+
+def random_user_id():
+    characters = string.ascii_letters + string.digits 
+    randomiser = "".join(random.choices(characters, k = 6))
+    return randomiser
+
+print(random_user_id())
+"""
+
+"""
+2. Modify the previous task. Declare a function named user_id_gen_by_user. It doesn't take any parameters but it takes two inputs using input(). One of the inputs is the number of characters and the second input is the number of IDs which are supposed to be generated.
+
+def user_id_gen_by_user():
+    characters_num = int(input("Enter the number of characters: "))
+    ID_num = int(input("how many id's would you like to generate?: "))
+    characters = string.ascii_letters + string.digits
+    
+    for randomiser in range(ID_num - 1):
+        randomiser = "".join(random.choices(characters, k = characters_num))
+        print(randomiser)
+    return randomiser 
+
+print(user_id_gen_by_user())
+"""
+
+"""
+3. Write a function named rgb_color_gen. It will generate rgb colors (3 values ranging from 0 to 255 each).
+
+def rgb_color_gen():
+    first_digit = random.randrange(0, 255)
+    second_digit = random.randrange(0, 255)
+    third_digit = random.randrange(0, 255)
+    return f"rgb({first_digit},{second_digit},{third_digit})"
+
+print(rgb_color_gen())
+"""
+
+"""
+Exercises: Level 2 
+1. Write a function list_of_hexa_colors which returns any number of hexadecimal colors in an array (six hexadecimal numbers written after #. Hexadecimal numeral system is made out of 16 symbols, 0-9 and first 6 letters of the alphabet, a-f. Check the task 6 for output examples).
+
+"""
+
+def list_of_hexa_colors(num):
+    letters = ["a", "b", "c", "d", "e", "f"]
+    characters = letters + string.digits
+    for hex in range(num):
+        hex = ''.join(random.choices(characters, k = 6))
+        print(f"")
