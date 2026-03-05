98534db02686ffcd6ec3185f29a3d8724524b1de
Bartek Gorczyca <1ej42.gorczyca@gmail.com>
2026-03-05 19:51:53 +0100

    day 12 completed

--- day_12.py
+++ day_12.py
@@ -95,3 +95,27 @@ def generate_colors(type, num):
 print(generate_colors("rgb", 3))
 """
 
+"""
+Exercises: Level 3
+1. Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+
+def shuffle_list(lst):
+    return random.sample(lst, len(lst))    
+    
+
+print(shuffle_list([1,2,3,4,5,6]))
+"""
+
+"""
+2. Write a function which returns an array of seven random numbers in a range of 0-9. All the numbers must be unique.
+
+def random_numbers():
+    lst = []
+    while len(lst) < 7:
+        num = random.randrange(0,9)
+        if num not in lst:
+            lst.append(num) 
+    return lst
+
+print(random_numbers())
+"""
